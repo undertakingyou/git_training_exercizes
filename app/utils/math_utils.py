f43bd4fb2efc8d959f9a18ec1a55c54089956292
Will Smith <undertakingyou@gmail.com>
2023-07-06 17:05:28 -0600

    Add the add, multiply, divide, double, square, and square_root functions to the math_utils.

---
+++ app/utils/math_utils.py
@@ -0,0 +1,23 @@
+import math
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def divide(a: int, b: int) -> int:
+    return a / b
+
+def double(a: int) -> int:
+    return a * 2
+
+def square(a: int) -> int:
+    return a * a
+
+def square_root(a: int) -> int:
+    return math.sqrt(a)
