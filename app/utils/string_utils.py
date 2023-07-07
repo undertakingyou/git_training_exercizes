e5ef12bb015fd2e8510e351481a6b407e0b4f8d5
Will Smith <undertakingyou@gmail.com>
2023-07-07 10:30:14 -0600

    WIP: Add a string utility

--- app/utils/string_utils.py
+++ app/utils/string_utils.py
@@ -1,2 +1,9 @@
 def concatenate(a: str, b: str) -> str:
     return a + ' ' + b
+
+def repeat(a: str, b: int) -> str:
+    results: str = ''
+    for i in range(b):
+        results += a
+
+    return results
