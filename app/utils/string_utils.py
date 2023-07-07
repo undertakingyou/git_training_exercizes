ec8ea53adde6368cc3e248547275d699e45a82ea
Will Smith <undertakingyou@gmail.com>
2023-07-06 18:04:21 -0600

    WHAT: * Add repeat string util * Update string utils and tests to use single quotes * Add tests for the string utils repeat function
    WHY:
    Because we wanted that

--- app/utils/string_utils.py
+++ app/utils/string_utils.py
@@ -1,2 +1,12 @@
 def concatenate(a: str, b: str) -> str:
-    return a + " " + b
+    return a + ' ' + b
+
+def repeat(a: str, b: int, spacer: str = None) -> str:
+    result: str = ''
+
+    for i in range(b):
+        result += a
+        if spacer and i != b - 1:
+            result += spacer
+
+    return result
--- tests/utils/string_utils_tests.py
+++ tests/utils/string_utils_tests.py
@@ -4,8 +4,24 @@ from app.utils import string_utils
 
 class MyTestCase(unittest.TestCase):
     def test_concatenate(self):
-        results = string_utils.concatenate("Hello", "there")
-        self.assertEqual(results, "Hello there")  # add assertion here
+        results = string_utils.concatenate('Hello', 'there')
+        self.assertEqual(results, 'Hello there')
+
+    def test_repeat(self):
+        results = string_utils.repeat('a', 3)
+        self.assertEqual(results, 'aaa')
+
+    def test_repeat_2(self):
+        results = string_utils.repeat('abc', 3)
+        self.assertEqual(results, 'abcabcabc')
+
+    def test_repeat_with_spacer(self):
+        results = string_utils.repeat('a', 3, 'b')
+        self.assertEqual(results, 'ababa')
+
+    def test_repeat_with_spacer_2(self):
+        results = string_utils.repeat('a', 3, ' ')
+        self.assertEquals(results, 'a a a')
 
 
 if __name__ == '__main__':
