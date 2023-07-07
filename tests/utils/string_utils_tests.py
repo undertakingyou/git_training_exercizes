91aa23908dee67022df6ca775698490eda448c9e
Will Smith <undertakingyou@gmail.com>
2023-07-07 10:34:21 -0600

    WIP: Add another string utils test.

--- tests/utils/string_utils_tests.py
+++ tests/utils/string_utils_tests.py
@@ -6,3 +6,11 @@ class MyTestCase(unittest.TestCase):
     def test_concatenate(self):
         results = string_utils.concatenate('Hello', 'world')
         self.assertEqual(results, 'Hello world')
+
+    def test_repeat(self):
+        results = string_utils.repeat('a', 3)
+        self.assertEqual(results, 'aaa')
+
+
+if __name__ == '__main__':
+    unittest.main()
